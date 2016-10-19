38ec9f27fb606ef823a5c652f387e05c988d8d2d
Ned Zimmerman <ned@bight.ca>
2016-10-19 14:40:58 -0300

    Refactor a little bit.

---
+++ pb-api.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Pressbooks REST API
+ * Description: JSON-based REST API for Pressbooks.
+ * Version: 1.0
+ * Plugin URI: https://github.com/pressbooks/pb-api/
+ * License: GPL2+
+ */
+
+/**
+ * Pressbooks\Modules\Api_v1\Api class.
+ */
+if ( ! class_exists( '\Pressbooks\Modules\Api_v1\Api' ) ) {
+	require_once dirname( __FILE__ ) . '/includes/modules/api_v1/class-pb-api.php';
+}
